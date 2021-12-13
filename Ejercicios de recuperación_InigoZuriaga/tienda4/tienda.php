4ab05a780a79950d06a0d8e6a097c02188252ace
ZuRi-To <91980222+ZuRi-To@users.noreply.github.com>
2021-12-13 02:40:31 +0100

    holu

--- Ejercicios de recuperación_InigoZuriaga/tienda4/tienda.php
+++ Ejercicios de recuperación_InigoZuriaga/tienda4/tienda.php
@@ -124,13 +124,6 @@
         </form>
     <?php } ?>
 
-    <a href="index.php" title="Volver">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-             width="2%">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6"/>
-        </svg>
-    </a>
 
 </body>
 </html>
